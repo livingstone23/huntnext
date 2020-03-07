503b1780cd2e2257c274fe264e76c6e29da222e3
Livingstone <livingstone23@gmail.com>
2020-03-07 03:48:33 -0600

    Creacion de proyectos y primeros pasos
    Creacion de Estructutura general y primeros componentes requeridos

---
+++ components/layout/Layout.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+
+
+const Layout = props => {
+
+    return (
+        <>
+            <h1>Header</h1>
+
+            <nav>
+                <Link href="/">Inicio</Link>
+                <Link href="/nosotros">Inicio</Link>
+            </nav>
+
+
+            <main>
+                {props.children}
+            </main>
+        </>
+    );
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout/Layout';
+
+
+const Home = () => (
+  <div>
+    <Layout>
+      <h1>Inicio</h1>
+    </Layout>
+  </div>
+)
+
+export default Home
