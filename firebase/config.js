0a33b692ce046f52ba67fbcfe3b1e8e2383371d6
Livingstone <livingstone23@gmail.com>
2020-03-19 01:49:28 -0600

    Actualizacion de componentes
    1- Aplicando Next, Firebase y el Header y su CSS,
    2- Creando el Login y la cuenta
    3- Creando Productos

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAI-WMSif7T20YMk_5PG9FUocg3PZLPgO8",
+    authDomain: "product-hunt-d5752.firebaseapp.com",
+    databaseURL: "https://product-hunt-d5752.firebaseio.com",
+    projectId: "product-hunt-d5752",
+    storageBucket: "product-hunt-d5752.appspot.com",
+    messagingSenderId: "1072934316632",
+    appId: "1:1072934316632:web:38a49b40d7405f3097f82d",
+    measurementId: "G-MTJSLZGF17"
+  };
+
+  export default firebaseConfig;
---
+++ validacion/validarCrearProducto.js
@@ -0,0 +1,31 @@
+export default function validarCrearProducto(valores) {
+
+    let errores = {};
+
+    //Validar el nombre del usuario
+    if(!valores.nombre) {
+        errores.nombre = 'El Nombre es obligatorio'
+    }
+
+    //Validar la empresa
+    if(!valores.empresa) {
+        errores.empresa = 'El nombre de empresa es obligatorio';
+    } 
+
+
+    //Validar URL
+     if(!valores.url) {
+        errores.url = 'La url del producto es obligatorio';
+    } else if( !/^(ftp|http|https):\/\/[^ "]+$/.test(valores.url) ) {
+        errores.url = "URL mal formateada o no válida";
+    }
+    
+    //Validar descripción
+    if(!valores.descripcion) {
+        errores.descripcion = 'Agrega una descripción de tu producto';
+    } 
+
+
+    return errores;
+
+}
