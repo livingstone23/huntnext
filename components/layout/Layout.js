7f1be04cbb00219eeab4936d93f7ff11a208f7a8
Livingstone <livingstone23@gmail.com>
2020-03-20 04:06:42 -0600

    Routing Dinamico y Paginas de Productos
    creacion de principales paginas para consulta, realizacion de votos y podes mostrar la informacion requerida

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,18 +1,72 @@
 import React from 'react';
-import Link from 'next/link';
+import Header from './Header';
+import { Global, css } from '@emotion/core';
+import Head from 'next/head';
 
 
 const Layout = props => {
 
     return (
         <>
-            <h1>Header</h1>
+            <Global 
+                styles={css`
+                    :root{
+                        --gris:#3d3d3d;
+                        --gris2:#6f6f6f;
+                        --gris3:#e1e1e1;
+                        --naranja:#DA552F;
+                    }
+                    
+                    html {
+                        font-size: 62.5%;
+                        box-sizing: border-box;
 
-            <nav>
-                <Link href="/">Inicio</Link>
-                <Link href="/nosotros">Inicio</Link>
-            </nav>
+                    }
+                    *, *:before, *:after {
+                        box-sizing: inherit;
+                    }
+                    body {
+                        font-size: 1.6rem; /** igual a 16 pixeles */
+                        line-height: 1.5;
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    h1,h2, h3 {
+                        margin: 0 0 2rem 0;
+                        line-height: 1.5;
+                    }
 
+                    h1,h2 {
+                        font-family: 'Roboto Slab', serif;
+                        font-weight: 700;
+                    }
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
+                    }
+
+
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                    a {
+                        text-decoration: none;
+                    }
+
+                    img {
+                        max-width: 100%;
+                    }
+                `}
+            />
+
+            <Head>
+                <title>Product Hunt Firebase y Next.js</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto+Slab:400,700&display=swap" rel="stylesheet"/>
+                <link href="/static/css/app.css" rel="stylesheet" />
+            </Head>
+
+            <Header />
 
             <main>
                 {props.children}
